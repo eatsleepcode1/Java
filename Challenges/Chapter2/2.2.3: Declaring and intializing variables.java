214c50ef7ee60123fa9023e19a7601ec3a7bf9b4
eatsleepcode1 <131639669+eatsleepcode1@users.noreply.github.com>
2023-04-24 22:16:12 -0400

    Create 2.2.3: Declaring and intializing variables.java

---
+++ Challenges/Chapter2/2.2.3: Declaring and intializing variables.java
@@ -0,0 +1,13 @@
+/*Write one statement that declares an integer variable numHouses initialized to 25. **/
+import java.util.Scanner;
+
+public class DeclaringVariables {
+   public static void main(String[] args) {
+
+      /* Your solution goes here  */
+
+      System.out.println(numHouses);
+   }
+}
+Solution:
+int numHouses = 25;
