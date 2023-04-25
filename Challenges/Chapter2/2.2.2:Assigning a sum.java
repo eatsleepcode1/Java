30b11e62f9f126da7eafbf480f76e472bfc541a5
eatsleepcode1 <131639669+eatsleepcode1@users.noreply.github.com>
2023-04-24 22:13:19 -0400

    Create 2.2.2:Assigning a sum.java

---
+++ Challenges/Chapter2/2.2.2:Assigning a sum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+/*Write a statement that assigns numCoins with numNickels + numDimes. Ex: 5 nickels and 6 dimes results in 11 coins. **/
+public class AssigningSum {
+   public static void main(String[] args) {
+      int numCoins;
+      int numNickels;
+      int numDimes;
+
+      numNickels = 5;
+      numDimes = 6;
+
+      /* Your solution goes here  */
+
+      System.out.print("There are ");
+      System.out.print(numCoins);
+      System.out.println(" coins");
+   }
+}
+Solution:
+numCoins= numNickels + numDimes;
