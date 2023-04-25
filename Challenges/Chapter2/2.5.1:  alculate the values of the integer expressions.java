4bbe2039c723a345124435a864689d22c6852e96
eatsleepcode1 <131639669+eatsleepcode1@users.noreply.github.com>
2023-04-24 22:24:55 -0400

    Create 2.5.1:  alculate the values of the integer expressions.java

---
+++ Challenges/Chapter2/2.5.1:  alculate the values of the integer expressions.java
@@ -0,0 +1,54 @@
+Level 2
+public class MathOperations {
+   public static void main(String[] args) {
+      int x;
+      int y;
+   
+      x = 10;
+      y = x * 2;  
+      System.out.println(y);
+   }
+}
+Output: 20
+Level 2
+public class MathOperations {
+   public static void main(String[] args) {
+      int x;
+      int y;
+      int z;
+
+      x = 7;
+      y = 1;
+      z = (4 + (x / 1)) - y;
+      System.out.println(z);
+   }
+}
+Output: 10
+Level 3
+public class MathOperations {
+   public static void main(String[] args) {
+      int x;
+
+      x = 9;
+      x += 5;
+      System.out.println(x);
+   }
+}
+Output: 14
+Level 5
+public class MathOperations {
+   public static void main(String[] args) {
+      int x;
+      int y;
+
+      x = 5;
+      y = 7;
+      x += (3 * y);
+      y = 6;
+      System.out.println(x);
+      System.out.println(y);
+   }
+}
+Output: 
+26
+ 6
