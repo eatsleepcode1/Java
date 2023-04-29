6df09def03d0e88602709a80b64de898a3deb5d1
eatsleepcode1 <131639669+eatsleepcode1@users.noreply.github.com>
2023-04-29 10:39:22 -0400

    Update 9.20: Print student roster.java

--- Labs/Chapter9/9.20: Print student roster.java
+++ Labs/Chapter9/9.20: Print student roster.java
@@ -15,6 +15,36 @@ Brenda Stern (GPA: 2.0)
 Jane Flynn (GPA: 3.9)
 Lynda Robison (GPA: 3.2)
 Students: 4*/
+import java.text.DecimalFormat;
+
+// Class representing a student
+public class Student {
+
+	private String first;  // first name
+	private String last;   // last name
+	private double gpa;    // grade point average
+
+	// Student class constructor
+	public Student(String first, String last, double gpa) {
+		this.first = first; // first name
+		this.last = last;   // last name
+		this.gpa = gpa;     // grade point average
+	}
+
+	public double getGPA() {
+		return gpa;
+	}
+
+	public String getLast() {
+		return last;
+	}
+
+	// Returns a String representation of the Student object, with the GPA formatted to one decimal
+	public String toString() {
+		DecimalFormat fmt = new DecimalFormat("#.0");
+		return first + " " + last + " " + "(GPA: " + fmt.format(gpa) + ")";
+	}
+}
 import java.util.ArrayList;
 
 // Class representing a student
