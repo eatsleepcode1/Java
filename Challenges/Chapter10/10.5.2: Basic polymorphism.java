84e31e8b9f3c22aa98c010b32db3d6a938fe0e71
eatsleepcode1 <131639669+eatsleepcode1@users.noreply.github.com>
2023-05-06 19:12:19 -0400

    Create 10.5.2: Basic polymorphism.java

---
+++ Challenges/Chapter10/10.5.2: Basic polymorphism.java
@@ -0,0 +1,66 @@
+/*Write the printItem() method for the base class. Sample output for below program:
+Last name: Smith
+First and last name: Bill Jones */
+// ===== Code from file BaseItem.java =====
+public class BaseItem {
+   protected String lastName;
+
+   public void setLastName(String providedName) {
+      lastName = providedName;
+   }
+
+   // FIXME: Define printItem() method
+
+   /* Your solution goes here  */
+
+}
+// ===== end =====
+
+// ===== Code from file DerivedItem.java =====
+public class DerivedItem extends BaseItem {
+   private String firstName;
+
+   public void setFirstName(String providedName) {
+      firstName = providedName;
+   }
+
+   @Override
+   public void printItem() {
+      System.out.print("First and last name: ");
+      System.out.println(firstName + " " + lastName);
+   }
+}
+// ===== end =====
+
+// ===== Code from file ItemInventory.java =====
+import java.util.ArrayList;
+
+public class ItemInventory {
+   public static void main(String[] args) {
+      BaseItem baseItemPtr;
+      DerivedItem derivedItemPtr;
+      ArrayList<BaseItem> itemList = new ArrayList<BaseItem>();
+      int i;
+
+      baseItemPtr = new BaseItem();
+      baseItemPtr.setLastName("Smith");
+
+      derivedItemPtr = new DerivedItem();
+      derivedItemPtr.setLastName("Jones");
+      derivedItemPtr.setFirstName("Bill");
+
+      itemList.add(baseItemPtr);
+      itemList.add(derivedItemPtr);
+
+      for (i = 0; i < itemList.size(); ++i) {
+         itemList.get(i).printItem();
+      }
+   }
+}
+// ===== end =====
+// FIXME: Define printItem() method
+
+   /* Your solution goes here  */
+public void printItem() {
+       System.out.println("Last name: " + lastName);
+    }
