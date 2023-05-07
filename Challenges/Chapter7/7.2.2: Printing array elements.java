742c8d2da62690314dbf55470799c0942f655550
eatsleepcode1 <131639669+eatsleepcode1@users.noreply.github.com>
2023-05-07 17:14:57 -0400

    Create 7.2.2: Printing array elements.java

---
+++ Challenges/Chapter7/7.2.2: Printing array elements.java
@@ -0,0 +1,24 @@
+/*Write three statements to print the first three elements of array runTimes. Follow each statement with a newline. 
+Ex: If runTimes = {800, 775, 790, 805, 808}, print:
+800 
+775 
+790 */
+import java.util.Scanner;
+
+public class PrintRunTimes {
+   public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      final int NUM_ELEMENTS = 5;
+      int [] runTimes = new int[NUM_ELEMENTS];
+      int i;
+
+      for (i = 0; i < runTimes.length; ++i) {
+         runTimes[i] = scnr.nextInt();
+      }
+
+      /* Your solution goes here  */
+      System.out.println(runTimes[0]);
+      System.out.println(runTimes[1]);
+      System.out.println(runTimes[2]);
+   }
+}
