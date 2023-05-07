024a66b6b22d914b08419f3c1d653703ff74ad54
eatsleepcode1 <131639669+eatsleepcode1@users.noreply.github.com>
2023-05-06 21:43:46 -0400

    Create 5.14.1: String comparison: Detect word.java

---
+++ Challenges/Chapter5/5.14.1: String comparison: Detect word.java
@@ -0,0 +1,19 @@
+/*Write an if-else statement that prints "Goodbye" if userString is "Quit", else prints "Hello". End with newline. */
+import java.util.Scanner;
+
+public class DetectWord {
+   public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      String userString;
+
+      userString = scnr.next();
+
+      /* Your solution goes here  */
+      if(userString.equals("Quit")){
+         System.out.println("Goodbye");
+         }
+      else{
+         System.out.println("Hello");
+         }
+   }
+}
