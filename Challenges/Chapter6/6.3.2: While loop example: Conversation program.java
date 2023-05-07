e6d77cee5f9f2a29733c851c257b8f6e0d90dbfb
eatsleepcode1 <131639669+eatsleepcode1@users.noreply.github.com>
2023-05-07 14:06:36 -0400

    Create 6.3.2: While loop example: Conversation program.java

---
+++ Challenges/Chapter6/6.3.2: While loop example: Conversation program.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+/* Program that has a conversation with the user. 
+   Uses a if-else statements and a random number (sort of) 
+   to mix up the program's responses. */
+
+public class Conversation {
+
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int randNum;       // Random number 0 to 3
+      String userText;      // User input
+
+      System.out.println("Tell me something about yourself.");
+      System.out.println("You can type \"Goodbye\" at anytime to quit.\n");
+      System.out.println();
+      System.out.print("> ");
+
+      userText = scnr.nextLine();
+
+      while (!userText.equals("Goodbye")) {
+         randNum = userText.length() % 4; // "Random" num. %4 ensures 0-3
+         System.out.println();
+
+         if (randNum == 0) {
+            System.out.println("Please explain further.");
+         }
+         else if (randNum == 1) {
+            System.out.println("Why do you say: \"" + userText + "\"?");
+         }
+         else if (randNum == 2) {
+            System.out.println("I don't think that's right.");
+         }
+         else if (randNum == 3) {
+            System.out.println("What else can you share?");
+         }
+         else {
+            System.out.println("Uh-oh, something went wrong. Try again.");
+         }
+         System.out.println();
+                   System.out.print("> ");
+         userText = scnr.nextLine();
+      }
+      System.out.println();
+
+      System.out.println("It was nice talking with you. Goodbye.");
+   }
+}
+//Output 
+/*Tell me something about yourself.
+You can type "Goodbye" at anytime to quit.
+
+
+> I'm 26 years old.
+
+Why do you say: "I'm 26 years old."?
+
+> Well, I was born 26 years ago.
+
+I don't think that's right.
+
+> I am sure it is correct.
+
+Please explain further.
+
+> Goodbye
+It was nice talking with you. Goodbye.*/
+
