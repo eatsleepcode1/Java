03ff0f91439f470f04fa81eefb971742d1c6388c
eatsleepcode1 <131639669+eatsleepcode1@users.noreply.github.com>
2024-02-14 16:12:12 -0500

    Create Entrants.java

---
+++ Project/Project4/Entrants.java
@@ -0,0 +1,8 @@
+public class Entrants {//class 1
+    private String name;
+    private String email;
+    private int JDK;
+    public final static int PRIZE = 3;
+    public final static int PRIZE1 = 3;
+  
+    }
