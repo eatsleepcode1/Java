ffaad4ef3bf03dd433f91305177ef1e427ea0497
eatsleepcode1 <131639669+eatsleepcode1@users.noreply.github.com>
2024-02-14 14:14:29 -0500

    Update Project1.java

--- Project/Project1.java
+++ Project/Project1.java
@@ -1,1 +1,31 @@
+//Fix the noted FIXME issues: change the hourlyWage amount so that it accurately reflects the hourly wage you would like to 
+//earn as a software developer, and also fix the monthly salary calculation so that the displayed result is correct.
+//public class Salary {                         
+   //public static void main(String[] args) { 
+      //int hourlyWage;
 
+      //hourlyWage = 20; // FIXME: Adjust to match your hourly earning goal as a software developer.
+                   
+      //System.out.print("Annual salary is: ");         
+      //System.out.println(hourlyWage * 40 * 50);   
+    
+      //System.out.print("Monthly salary is: ");
+      //System.out.println((hourlyWage * 40 * 50) / 1); 
+      // FIXME: The above calculation is wrong. Fix the calculation so that it prints monthly salary.
+
+   //} 
+//}
+public class Salary {
+    public static void main(String[] args)
+    {
+        int hourlyWage;
+        hourlyWage = 36;
+
+        System.out.print("Annual salary is: ");
+        System.out.println(hourlyWage * 40 * 50);
+
+        System.out.print("Monthly salary is: ");
+        System.out.println((hourlyWage * 40 * 50) /12);
+    }
+
+}
